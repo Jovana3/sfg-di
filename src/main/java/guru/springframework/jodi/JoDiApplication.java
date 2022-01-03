3bbaa3abb056a9270b8e219e35d3086d2b0f645e
veljoj <jovana.veljovic@msg-global.com>
2022-01-03 21:37:35 +0100

    Add init spring project

---
+++ src/main/java/guru/springframework/jodi/JoDiApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.jodi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoDiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/jodi/JoDiApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.jodi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
